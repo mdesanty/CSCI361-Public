405d6cc5b3c2b4fc8c11a6b101363e6a139ad10d
mclacsdev <mclacsdev@mcla.edu>
2016-09-19 18:42:21 -0400

    fixed bug in fibo

--- workspace/Recursion/src/Fibonacci.java
+++ workspace/Recursion/src/Fibonacci.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class Fibonacci 
+public class Fibonacci
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		// Get n from the user...
 		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter n:");
 		int n = in.nextInt();
-		
+
 		int fnum = fibonacci(n);
 		System.out.println(fnum);
 	}
@@ -17,7 +17,7 @@ public class Fibonacci
 	public static int fibonacci(int n)
 	{
 		if (n == 0)
-			return 1;
+			return 0;
 		else if (n == 1)
 			return 1;
 		else
