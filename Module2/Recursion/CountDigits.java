841c642b5ef275ef814b74f0c1894389167c9d4a
Mark Cohen <mark.cohen@mcla.edu>
2017-08-16 12:37:53 +0000

    changes

--- Module2/Recursion/CountDigits.java
+++ Module2/Recursion/CountDigits.java
@@ -7,7 +7,7 @@ public class CountDigits
 		// Get n from the user...
 		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter string of digits:");
+		System.out.println("Enter string:");
 		String s = in.nextLine();
 
 		int count = countDigits(s);
@@ -16,9 +16,13 @@ public class CountDigits
 
 	public static int countDigits(String s)
 	{
-		if (s.length() == 1)
+		if (s.length() == 0)
+			return 0;
+		else if (s.length() == 1 && Character.isDigit(s.charAt(0)))
 			return 1;
-		else
-			return 1 + countDigits(s.substring(1));
+		else if (s.length() == 1 && !Character.isDigit(s.charAt(0)))
+			return 0;
+		else 
+			return countDigits(s.substring(0,1)) + countDigits(s.substring(1, s.length()));
 	}
 }
